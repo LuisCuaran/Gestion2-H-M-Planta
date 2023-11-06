05e453a4987298fa50356cc871e24533d0858ff6
LuisCuaran <albert10luis@gmail.com>
2023-11-06 11:24:08 -0500

    2.0 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
+import { AdministracionComponent } from './modulos/administracion/administracion.component';
+import { RegistroComponent } from './modulos/registro/registro.component';
+import { MantenimientoComponent } from './modulos/mantenimiento/mantenimiento.component';
+import { InventarioComponent } from './modulos/inventario/inventario.component';
+import { ManualDeHerramientasComponent } from './modulos/manual.de.herramientas/manual.de.herramientas.component';
+
+const routes: Routes = [
+{
+  path: "", component: PrincipalComponent,
+  children: [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "", redirectTo: "/dashboard", pathMatch: "full"},
+  {path: "usuarios", component: UsuariosComponent},
+  {path: "administracion", component: AdministracionComponent},
+  {path: "registro", component: RegistroComponent},
+  {path: "mantenimiento", component: MantenimientoComponent},
+  {path: "inventarios", component: InventarioComponent},
+  {path: "manual_de_herramientas", component: ManualDeHerramientasComponent},
+  ]
+},
+{path: "login", component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './estructura/header/header.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
+import { AdministracionComponent } from './modulos/administracion/administracion.component';
+import { RegistroComponent } from './modulos/registro/registro.component';
+import { MantenimientoComponent } from './modulos/mantenimiento/mantenimiento.component';
+import { InventarioComponent } from './modulos/inventario/inventario.component';
+import { ManualDeHerramientasComponent } from './modulos/manual.de.herramientas/manual.de.herramientas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    LoginComponent,
+    UsuariosComponent,
+    AdministracionComponent,
+    RegistroComponent,
+    MantenimientoComponent,
+    InventarioComponent,
+    ManualDeHerramientasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
